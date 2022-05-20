7e176dad058a0dc65bfc471c1703deeda5a12aac
Clint <clintbugs.dev@gmail.com>
2022-05-20 11:45:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {useState} from "react"
+import ImageCropDialog from "./ImageCropDialog"
+
+const initData = [
+  {
+    id: 1,
+    imageUrl: "images/demo1.png",
+    croppedImageUrl: null,
+  },
+  {
+    id: 2,
+    imageUrl: "images/demo2.png",
+    croppedImageUrl: null,
+  },
+]
 
 function App() {
+  const [docs, setDocs] = useState(initData)
+  const [selectedDoc, setSelectedDoc] = useState(null)
+
+  const onCancel = () => {
+    setSelectedDoc(null)
+  }
+
+  const setCroppedImageFor = (id, crop, zoom, aspect, croppedImageUrl) => {
+    const newDocsList = [...docs]
+    const docIndex = docs.findIndex(x=>x.id === id)
+    const doc = docs[docIndex]
+    const newDoc = {...doc, croppedImageUrl, crop, zoom, aspect}
+    newDocsList[docIndex] = newDoc
+    setDocs(newDocsList)
+    setSelectedDoc(null)
+  }
+
+  const resetImage = (id) => {
+    setCroppedImageFor(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedDoc ? (
+        <ImageCropDialog 
+          id={selectedDoc.id} 
+          imageUrl={selectedDoc.imageUrl} 
+          cropInit={selectedDoc.crop} 
+          zoomInit={selectedDoc.zoom}
+          aspectInit={selectedDoc.aspect}
+          onCancel={onCancel}
+          setCroppedImageFor={setCroppedImageFor}
+          resetImage={resetImage}
+        />
+      ) : null}
+
+      {docs.map((doc) => (
+        <div className="imageCard" key={doc.id}>
+          <img 
+            src={doc.croppedImageUrl ? doc.croppedImageUrl : doc.imageUrl} 
+            alt="" 
+            onClick={() => {
+              console.log(doc)
+              setSelectedDoc(doc)
+            }} 
+            />
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
